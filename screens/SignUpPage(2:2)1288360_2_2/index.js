510543de0b6da0557f72f464dbda55bca168e625
Crowdbotics <team@crowdbotics.com>
2022-09-11 18:10:49 +0000

    Update components

---
+++ screens/SignUpPage(2:2)1288360_2_2/index.js
@@ -0,0 +1,229 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome"
+import { CheckBox } from "react-native-elements"
+import { connect } from "react-redux"
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen"
+import { getNavigationScreen } from "@screens"
+export class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  render = () => (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      style={styles.ScrollView_1}
+    >
+      <View style={styles.View_2} />
+      <View style={styles.View_2_3} />
+      <View style={styles.View_2_4}>
+        <Text style={styles.Text_2_4}>Create an account</Text>
+      </View>
+      <View style={styles.View_2_6}>
+        <Text style={styles.Text_2_6}>Email</Text>
+      </View>
+      <View style={styles.View_2_7} />
+      <View style={styles.View_2_8}>
+        <Text style={styles.Text_2_8}>Password</Text>
+      </View>
+      <View style={styles.View_2_9} />
+      <View style={styles.View_2_10}>
+        <Text style={styles.Text_2_10}>Confirm Password</Text>
+      </View>
+      <View style={styles.View_2_11} />
+      <View style={styles.View_2_12} />
+      <View style={styles.View_2_13}>
+        <Text style={styles.Text_2_13}>Sign up</Text>
+      </View>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  ScrollView_1: { backgroundColor: "rgba(255, 255, 255, 1)" },
+  View_2: { height: hp("122%") },
+  View_2_3: {
+    width: wp("100%"),
+    minWidth: wp("100%"),
+    height: hp("12%"),
+    minHeight: hp("12%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("0%"),
+    backgroundColor: "rgba(217, 217, 217, 1)"
+  },
+  View_2_4: {
+    width: wp("68%"),
+    minWidth: wp("68%"),
+    minHeight: hp("4%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("16%"),
+    top: hp("31%"),
+    justifyContent: "flex-start"
+  },
+  Text_2_4: {
+    color: "rgba(0, 0, 0, 1)",
+    fontSize: 24,
+    fontWeight: "400",
+    textAlign: "center",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  View_2_6: {
+    width: wp("8%"),
+    minWidth: wp("8%"),
+    minHeight: hp("2%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("12%"),
+    top: hp("46%"),
+    justifyContent: "flex-start"
+  },
+  Text_2_6: {
+    color: "rgba(0, 0, 0, 1)",
+    fontSize: 10,
+    fontWeight: "400",
+    textAlign: "center",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  View_2_7: {
+    width: wp("75%"),
+    minWidth: wp("75%"),
+    height: hp("4%"),
+    minHeight: hp("4%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("11%"),
+    top: hp("52%"),
+    backgroundColor: "rgba(217, 217, 217, 1)"
+  },
+  View_2_8: {
+    width: wp("14%"),
+    minWidth: wp("14%"),
+    minHeight: hp("2%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("12%"),
+    top: hp("61%"),
+    justifyContent: "flex-start"
+  },
+  Text_2_8: {
+    color: "rgba(0, 0, 0, 1)",
+    fontSize: 10,
+    fontWeight: "400",
+    textAlign: "center",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  View_2_9: {
+    width: wp("75%"),
+    minWidth: wp("75%"),
+    height: hp("4%"),
+    minHeight: hp("4%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("11%"),
+    top: hp("67%"),
+    backgroundColor: "rgba(217, 217, 217, 1)"
+  },
+  View_2_10: {
+    width: wp("27%"),
+    minWidth: wp("27%"),
+    minHeight: hp("2%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("12%"),
+    top: hp("78%"),
+    justifyContent: "flex-start"
+  },
+  Text_2_10: {
+    color: "rgba(0, 0, 0, 1)",
+    fontSize: 10,
+    fontWeight: "400",
+    textAlign: "center",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  View_2_11: {
+    width: wp("75%"),
+    minWidth: wp("75%"),
+    height: hp("4%"),
+    minHeight: hp("4%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("11%"),
+    top: hp("83%"),
+    backgroundColor: "rgba(217, 217, 217, 1)"
+  },
+  View_2_12: {
+    width: wp("60%"),
+    minWidth: wp("60%"),
+    height: hp("8%"),
+    minHeight: hp("8%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("17%"),
+    top: hp("98%"),
+    backgroundColor: "rgba(104, 103, 103, 1)"
+  },
+  View_2_13: {
+    width: wp("11%"),
+    minWidth: wp("11%"),
+    minHeight: hp("2%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("42%"),
+    top: hp("101%"),
+    justifyContent: "flex-start"
+  },
+  Text_2_13: {
+    color: "rgba(255, 255, 255, 1)",
+    fontSize: 10,
+    fontWeight: "400",
+    textAlign: "center",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  }
+})
+
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
