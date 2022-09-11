fce949936666ee3ab880ff82b9d34370c10e5021
Crowdbotics <team@crowdbotics.com>
2022-09-11 18:16:26 +0000

    Update models

--- backend/calm_exercises_37047/wsgi.py
+++ backend/calm_exercises_37047/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calm_exercises_37047.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "calm_exercises_37047.settings")
 
 application = get_wsgi_application()
